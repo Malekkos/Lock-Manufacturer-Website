7806390307681d9a2603122b0f5836a3074d4aa3
Christian Anderson <tehninjawaffle@gmail.com>
2024-05-22 15:22:20 -0700

    Added text explaining project, Added and changed some styling as well as included some media query

--- src/js/components/home.jsx
+++ src/js/components/home.jsx
@@ -7,15 +7,15 @@ import React from "react";
 function Home () {
 
   return (
-    <div>
+    <>
       <div id="lock-handle">
         <div className="left-arc"></div>
         <div className="right-arc"></div>
       </div>
-      {/* <div id="proj-explanation">
-        <h4> This is a test paragraph AHAHHAHA</h4>
-      </div> */}
-    </div>
+      <div id="proj-explanation">
+        <h4 className="text">Thanks for visiting! This is a test project to understand some new concepts. In this instance, they are SASS, Vite, Bootstrap, and carousels. Enjoy!</h4>
+      </div>
+    </>
   )
 }
 
