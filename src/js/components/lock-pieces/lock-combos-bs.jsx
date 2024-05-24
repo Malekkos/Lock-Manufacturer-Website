58f1270086cc47b762a91d022e47418127c1b4c1
Christian Anderson <tehninjawaffle@gmail.com>
2024-05-24 12:30:58 -0700

    Added alt's for the pictures

--- src/js/components/lock-pieces/lock-combos-bs.jsx
+++ src/js/components/lock-pieces/lock-combos-bs.jsx
@@ -30,22 +30,22 @@ function BSCarousel(id) {
         <div id={carousel_id} className="carousel slide">
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img src="https://cdn.masterlock.com/product/orig/MLCOM_PRODUCT_5.jpg" className="d-block w-100" alt="..." />
+              <img src="https://cdn.masterlock.com/product/orig/MLCOM_PRODUCT_5.jpg" className="d-block w-100" alt="Standard silver lock with keys" />
             </div>
             <div className="carousel-item special">
-              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_6121.jpg" className="d-block w-100" alt="..." />
+              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_6121.jpg" className="d-block w-100" alt="Large lock with black body, ProSeries" />
             </div>
             <div className="carousel-item">
-              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_6271.jpg" className="d-block w-100" alt="..." />
+              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_6271.jpg" className="d-block w-100" alt="Fully circular lock, silvery" />
             </div>
             <div className="carousel-item">
-              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_A700.jpg" className="d-block w-100" alt="..." />
+              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_A700.jpg" className="d-block w-100" alt="Dumbell shaped lock, silvery" />
             </div>
             <div className="carousel-item">
-              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_101.jpg" className="d-block w-100" alt="..." />
+              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_101.jpg" className="d-block w-100" alt="Thin lock with key, large loop on top" />
             </div>
             <div className="carousel-item">
-              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_1UP.jpg" className="d-block w-100" alt="..." />
+              <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_1UP.jpg" className="d-block w-100" alt="Standard silver lock, blue lining on the bottom" />
             </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target={`#${carousel_id}`} data-bs-slide="prev">
