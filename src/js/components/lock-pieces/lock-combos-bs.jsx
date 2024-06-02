e18fdfbdf58bafdf6d8408dd48dae6c97a417710
Christian Anderson <tehninjawaffle@gmail.com>
2024-06-02 13:12:11 -0700

    Added another styling to the sass carousel tree

--- src/js/components/lock-pieces/lock-combos-bs.jsx
+++ src/js/components/lock-pieces/lock-combos-bs.jsx
@@ -28,9 +28,11 @@ function BSCarousel(id) {
       <div>
         <div id={carousel_id} className="carousel slide">
           <div className="carousel-inner">
+            <div className="carousel-hider"></div>
             <div className="carousel-item active">
               <img src="https://cdn.masterlock.com/product/orig/MLCOM_PRODUCT_5.jpg" className="d-block w-100" alt="Standard silver lock with keys" />
             </div>
+            <div className="carousel-hider"></div>
             <div className="carousel-item special">
               <img src="https://cdn.masterlock.com/product/285/MLCOM_PRODUCT_6121.jpg" className="d-block w-100" alt="Large lock with black body, ProSeries" />
             </div>
