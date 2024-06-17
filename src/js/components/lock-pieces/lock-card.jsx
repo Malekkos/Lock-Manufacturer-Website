d0e8089b25d672ef6f4facd113124fadd96d9fc4
Christian Anderson <tehninjawaffle@gmail.com>
2024-06-17 15:06:38 -0700

    Currently, all the planned animations are in. They just aren't working, though. I made a mistake with introducing the cards first. Gonna remove them and try to get back to original place and THEN implemnent the cards

--- src/js/components/lock-pieces/lock-card.jsx
+++ src/js/components/lock-pieces/lock-card.jsx
@@ -7,11 +7,11 @@ function LockCard(cardData) {
   return (
     <div className="card">
       <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body" id="cardBody">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
+      <div className="card-body" id="cardBody">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
   )
 }
--- src/js/components/lock-pieces/lock-combos-org.jsx
+++ src/js/components/lock-pieces/lock-combos-org.jsx
@@ -13,6 +13,56 @@ import LockCard from "./lock-card";
 // cont.    component for the card, because its a large element field.
 
 function OrgCarousel() {
+  // The following animation work is from this posting: https://medium.com/@zenab.awada/creating-a-draggable-carousel-using-javascript-a84337110808
+
+
+
+  window.onload = function () {
+    let container = document.querySelector(".orgContainer")
+    let innerContainer = document.querySelector(".orgContainerData")
+
+    let pressed = false;
+    let startX;
+    let x;
+
+    let boundItems = () => {
+      let outer = container.getBoundingClientRect();
+      let inner = innerContainer.getBoundingClientRect();
+
+      if (parseInt(innerContainer.style.left) > 0) {
+        innerContainer.style.left = "0px";
+      }
+
+      if (inner.right < outer.right) {
+        innerContainer.style.left = `-${inner.width - outer.width}px`
+      }
+    }
+
+    container.addEventListener("mousedown", (event) => {
+      pressed = true;
+      startX = event.offsetX - innerContainer.offsetLeft;
+      container.style.cursor = "grabbing";
+    });
+
+    container.addEventListener("mouseenter", () => {
+      container.style.cursor = "grab";
+    });
+
+    container.addEventListener("mouseup", () => {
+      container.style.cursor = "grab";
+      pressed = false;
+    });
+
+    container.addEventListener("mousemove", (event) => {
+      if (!pressed) return;
+      event.preventDefault();
+
+      x = event.offsetX;
+      innerContainer.style.left = `${x - startX}px`;
+      boundItems();
+    });
+  }
+
   return (
     <div className="orgContainer">
       <div className="orgContainerData">
