4a9c0785fc42826000809cc347b19022959dee7a
Christian Anderson <tehninjawaffle@gmail.com>
2024-06-22 17:45:01 -0700

    Cleaned up code, spaced it out, removed consoles and no longer functionial code

--- src/js/components/lock-pieces/lock-combos-org.jsx
+++ src/js/components/lock-pieces/lock-combos-org.jsx
@@ -19,58 +19,57 @@ function OrgCarousel() {
 
   window.onload = function () {
     let container = document.querySelectorAll(".orgContainer")
-    let innerContainer = document.querySelector(".orgContainerData")
 
     let pressed = false;
     let startX;
     let x;
-    console.log(container)
-    
+
 
     container.forEach(element => {
+      let innerContainerLocal = element.firstChild
+
       let boundItems = () => {
         let outer = element.getBoundingClientRect();
-        let inner = element.firstChild.getBoundingClientRect();
-  
-        if (parseInt(element.firstChild.style.left) > 0) {
-          element.firstChild.style.left = "0px";
+        let inner = innerContainerLocal.getBoundingClientRect();
+
+        if (parseInt(innerContainerLocal.style.left) > 0) {
+          innerContainerLocal.style.left = "0px";
         }
-  
+
         if (inner.right < outer.right) {
-          element.firstChild.style.left = `-${inner.width - outer.width}px`
+          innerContainerLocal.style.left = `-${inner.width - outer.width}px`
         }
       }
-      console.log(element.firstChild)
-      let innerContainerLocal = element.firstChild
+
       element.addEventListener("mousedown", (event) => {
         pressed = true;
         startX = event.offsetX - innerContainerLocal.offsetLeft;
         element.style.cursor = "grabbing";
       });
-  
+
       element.addEventListener("mouseenter", () => {
         element.style.cursor = "grab";
       });
-  
+
       element.addEventListener("mouseup", () => {
         element.style.cursor = "grab";
         pressed = false;
       });
-  
+
       element.addEventListener("mouseleave", () => {
         pressed = false;
       })
-  
+
       element.addEventListener("mousemove", (event) => {
         if (!pressed) return;
         event.preventDefault();
-  
+
         x = event.offsetX;
         innerContainerLocal.style.left = `${x - startX}px`;
         boundItems();
       });
     });
-    
+
   }
 
   return (
