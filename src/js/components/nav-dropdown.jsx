e5de1d1957d36f8d3f83c597dd6f833d7293dbf4
Christian Anderson <tehninjawaffle@gmail.com>
2024-06-30 10:21:20 -0700

    Setup nav, imported into home, setup CSS

---
+++ src/js/components/nav-dropdown.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// ≡≡
+function Nav() {
+
+  const drop = () => {
+    document.getElementById("dd").classList.toggle("show");
+  }
+
+  return (
+    <div id="nav-container">
+      <button onClick={() => drop()} className="ddbutton">≡</button>
+      <div id="dd" className="ddcontent">
+        <a href="#">Home</a>
+        <a href="#">Cart</a>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
